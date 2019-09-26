186452614b992e9f333f6b37c90e99ceb9463fb0
Gabriela <mg.paez@uniandes.edu.co>
2019-09-25 23:51:06 -0500

    Pruebas

--- test/test/logic/TestColas.java
+++ test/test/logic/TestColas.java
@@ -3,10 +3,15 @@ package test.logic;
 import java.util.ArrayList;
 
 import junit.framework.TestCase;
+import model.data_structures.MaxColaCP;
+import model.data_structures.MaxHeapCP;
 import model.logic.TravelTime; 
-
+/**
+ *@Test
+ */
 public class TestColas extends TestCase 
 {
+	
    ArrayList<TravelTime> casosPrueba = new ArrayList<TravelTime>();
    
    TravelTime travel1 = new TravelTime(0000, 0124, 11, 20, 3.9,69.5, 2.3);
@@ -18,6 +23,25 @@ public class TestColas extends TestCase
    TravelTime travel4 = new TravelTime(0104, 0133, 7, 5, 3.6,6.51, 2.8);
    
    TravelTime travel5 = new TravelTime(800, 0223, 4, 5, 3.4,65, 2.95);
+   
+   @SuppressWarnings("rawtypes")
+   MaxColaCP testCola = new MaxColaCP();
+   
+   @SuppressWarnings("rawtypes")
+   MaxHeapCP testHeap = new  MaxHeapCP(); 
+   
+   testCola.agregar(travel1);
+   testCola.agregar(travel2);
+   testCola.agregar(travel3);
+   testCola.agregar(travel4);
+   testCola.agregar(travel5);
+   
+   testHeap.agregar(travel1);
+   testHeap.agregar(travel2);
+   testHeap.agregar(travel3);
+   testHeap.agregar(travel4);
+   testHeap.agregar(travel5);
+   
    
    
 }
